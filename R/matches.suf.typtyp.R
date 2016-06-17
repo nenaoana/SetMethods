bb38cf003333b7143cb56eea34210b8dce0871d5
nenaoana <nena.oana@yahoo.com>
2016-06-17 13:04:54 +0200

    Update matches.suf.typtyp.R

--- R/matches.suf.typtyp.R
+++ R/matches.suf.typtyp.R
@@ -6,7 +6,7 @@ function(results,
            intermed=FALSE,
            sol=1,
            max_pairs=5)
-    
+
   {
     pdata <- pimdata(results=results, outcome=outcome, intermed=intermed, sol=sol)
     outcome <- toupper(outcome)
@@ -15,27 +15,29 @@ function(results,
     data1 <- data.frame(matrix(NA,ncol=0,nrow=nrow(data)))
     row.names(data1)<-row.names(data)
     tl <- gsub('\\s', '', nterm)
-    tl <- strsplit(tl, '\\*') 
-    tn <- unique(unlist(tl)) 
-    t_pre <- toupper(tn)[toupper(tn)==tn] 
-    t_neg <- toupper(tn)[tolower(tn)==tn] 
+    tl <- strsplit(tl, '\\*')
+    tn <- unique(unlist(tl))
+    t_pre <- toupper(tn)[toupper(tn)==tn]
+    t_neg <- toupper(tn)[tolower(tn)==tn]
     if (length(t_pre) > 0) {
       data1[t_pre] <- data[t_pre]
-      colnames(data1[t_pre])<-toupper(colnames(data1[t_pre]))        
+      colnames(data1[t_pre])<-toupper(colnames(data1[t_pre]))
     }
     if (length(t_neg) > 0) {
-      data1[t_neg] <- 1 - data[t_neg] 
+      data1[t_neg] <- 1 - data[t_neg]
       colnames(data1[t_neg])<-tolower(colnames(data1[t_neg]))
     }
-    
+
     for (i in (1:length(tn)))
-    { print(paste("Focal Conjunct", tn[i], sep = " "))                                          
-      x <- data1[toupper(tn[i])] 
+    { print(paste("Focal Conjunct", tn[i], sep = " "))
+      x <- data1[toupper(tn[i])]
       if (!neg.out){
-        y <- data[outcome]}  
+        y <- data[outcome]}
       else{
         y <- 1-data[outcome]
-      } 
+      }
+    if (length(tn)==1) {print(paste("This term has a single condition!"))}
+      else{
       co<- tn[-grep(tn[i], tn)]
       co<- toupper(co)
       codata<-data1[co]
@@ -47,20 +49,20 @@ function(results,
         codata1<-codata
         names(codata1)[1]<-"a"}
       codata$term<-pmin(codata1$a,x[,])
-      
+
       typical <-(codata$term>0.5) & (y>0.5) & (codata$term<=y)
       typ1 <- (x <= codata1$a)
       typ2 <- (x > codata1$a)
-      
+
       d <- rownames(data1)[typical]
       e <- rownames(data1)[typical]
       if (identical(e, character(0))) {print("no typical cases")}
-      else { 
+      else {
       casesm<-c()
       casesn<-c()
       val<-c()
       order<-c()
-      for (m in d) { 
+      for (m in d) {
         for (n in e) {
           s <- (((y[m,]-x[m,])+(y[n,]-x[n,])+(1.5-(x[m,]+x[n,])))/((x[m,]+x[n,])))
           sm<-((y[m,]-codata[m,"term"])/codata[m,"term"])
@@ -76,7 +78,7 @@ function(results,
               }
             }
           }
-        }} 
+        }}
       matcres <- data.frame(casesm, casesn, val, order)
       matcres[,5] <- NA
       matcres[,6] <- NA
@@ -88,7 +90,9 @@ function(results,
       }}
       maxl<-min(max_pairs,nrow(matcres))
       matcres<-matcres[order(matcres$PairRank,-matcres$UniqCov1, -matcres$UniqCov2, matcres$Distance),]
-      print(head(matcres, maxl))       
-      }  
+      print(head(matcres, maxl))
+      }
+      }
     }
-  }
+}
+
