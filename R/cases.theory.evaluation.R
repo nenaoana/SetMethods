9dde33d06c568bd479a004b4a343c656cec8a1ee
nenaoana <nena.oana@yahoo.com>
2016-07-25 22:21:41 +0300

    Update cases.theory.evaluation.R

--- R/cases.theory.evaluation.R
+++ R/cases.theory.evaluation.R
@@ -1,14 +1,13 @@
 cases.theory.evaluation <-
-function(theory, empirics, outcome, intermed=FALSE, sol=1)
+function(theory_eval)
   {
-    ND <- theory.evaluation(theory=theory, empirics=empirics, outcome=outcome, 
-                               intermed=intermed, sol=sol)
-    list('Covered Most Likely (T*E and Y > 0.5)'=rownames(ND)[(ND$'T*E'>0.5)&(ND[,outcome]>0.5)],
-         'Covered Least Likely (t*E and Y > 0.5)'=rownames(ND)[(ND$'t*E'>0.5)&(ND[,outcome]>0.5)],
-         'Uncovered Most Likely (T*e and Y > 0.5)'=rownames(ND)[(ND$'T*e'>0.5)&(ND[,outcome]>0.5)],
-         'Uncovered Least Likely (t*e and Y > 0.5)'=rownames(ND)[(ND$'t*e'>0.5)&(ND[,outcome]>0.5)],
-         'Inconsistent Most Likely (T*E and Y < 0.5)'=rownames(ND)[(ND$'T*E'>0.5)&(ND[,outcome]<0.5)],
-         'Inconsistent Least Likely (t*E and Y < 0.5)'=rownames(ND)[(ND$'t*E'>0.5)&(ND[,outcome]<0.5)],
-         'Consistent Most Likely (T*e and Y < 0.5)'=rownames(ND)[(ND$'T*e'>0.5)&(ND[,outcome]<0.5)],
-         'Consistent Least Likely (t*e and Y < 0.5)'=rownames(ND)[(ND$'t*e'>0.5)&(ND[,outcome]<0.5)])
+    ND <- theory_eval
+    list('Covered Most Likely (T*E and Y > 0.5)'=rownames(ND)[(ND$'T*E'>0.5)&(ND$'Outcome'>0.5)],
+         'Covered Least Likely (t*E and Y > 0.5)'=rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'>0.5)],
+         'Uncovered Most Likely (T*e and Y > 0.5)'=rownames(ND)[(ND$'T*e'>0.5)&(ND$'Outcome'>0.5)],
+         'Uncovered Least Likely (t*e and Y > 0.5)'=rownames(ND)[(ND$'t*e'>0.5)&(ND$'Outcome'>0.5)],
+         'Inconsistent Most Likely (T*E and Y < 0.5)'=rownames(ND)[(ND$'T*E'>0.5)&(ND$'Outcome'<0.5)],
+         'Inconsistent Least Likely (t*E and Y < 0.5)'=rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'<0.5)],
+         'Consistent Most Likely (T*e and Y < 0.5)'=rownames(ND)[(ND$'T*e'>0.5)&(ND$'Outcome'<0.5)],
+         'Consistent Least Likely (t*e and Y < 0.5)'=rownames(ND)[(ND$'t*e'>0.5)&(ND$'Outcome'<0.5)])
   }
