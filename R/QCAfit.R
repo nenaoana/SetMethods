285a51c10cab820d7094c0b3e48a8ae9c2b0bbb5
nenaoana <nena.oana@yahoo.com>
2017-02-06 13:29:44 +0100

    Update QCAfit.R

--- R/QCAfit.R
+++ R/QCAfit.R
@@ -5,7 +5,7 @@ function(x, y, cond.lab = NULL, necessity = FALSE, negation = FALSE){
 	
 	v <- matrix(NA, length(x[ ,1]), length(x[1, ]))
 	
-	out <- matrix(NA, length(x[1, ]), 7)
+	out <- matrix(NA, length(x[1, ]), 8)
 	
 	if(negation == TRUE){
 		y <- 1 - y
@@ -14,22 +14,24 @@ function(x, y, cond.lab = NULL, necessity = FALSE, negation = FALSE){
 	for(i in 1:length(x[1,])){
 		v[, i] <- pmin(x[, i], y) 
 		
+		
 		out[i, 1] <- sum(v[, i])/sum(x[, i]) 			# Con. suf.
 		out[i, 2] <- sum(v[, i])/sum(y)		 			# Cov. suf.
 		out[i, 3] <- sum(v[, i])/sum(y)		 			# Con. nec.
 		out[i, 4] <- sum(v[, i])/sum(x[, i]) 			# Cov. nec.
-		out[i, 5] <- sum(1 - x[, i])/sum(1 - v[, i])	# RoV
+		out[i, 5] <- sum(1 - x[, i])/sum(1 - v[, i])	# RoN
 		
 		p1 <- sum(pmin(x[, i], y)) - sum(pmin(x[, i], y, 1 - y))
 		p2 <- sum(x[, i]) - sum(pmin(x[, i], y, 1 - y))
+		p3 <- sum(v[, i] + sqrt(pmax((x[, i]-y),0)*x[, i]))
 		
 		out[i, 6] <- p1/p2								# PRI		
 		out[i, 7] <- out[i, 1] * out[i, 6]				# PRODUCT
-		
+		out[i, 8] <- sum(v[, i])/p3 # Haesebrouck Consistency Measure
 		}
 	
-	suf <- matrix(out[, c(1:2, 6:7)], nrow = ncol(x))
-	colnames(suf) <- c("Cons. Suf.", "Cov. Suf.", "PRI", "PRODUCT")					  					   
+	suf <- matrix(out[, c(1:2, 6:8)], nrow = ncol(x))
+	colnames(suf) <- c("Cons. Suf.", "Cov. Suf.", "PRI", "PRODUCT", "Haesebrouck Cons.")					  					   
 	rownames(suf) <- cond.lab
 	suf <- format(suf, digits = 3)
 	storage.mode(suf) <- "numeric"
