a44edaa46521ba173385a4711dec40572a80dad0
nenaoana <nena.oana@yahoo.com>
2017-02-23 19:44:30 +0100

    Update cases.theory.evaluation.R

--- R/cases.theory.evaluation.R
+++ R/cases.theory.evaluation.R
@@ -1,13 +1,55 @@
 cases.theory.evaluation <-
-function(theory_eval)
+  function(theory_eval)
   {
     ND <- theory_eval
-    list('Covered Most Likely (T*E and Y > 0.5)'=rownames(ND)[(ND$'T*E'>0.5)&(ND$'Outcome'>0.5)],
-         'Covered Least Likely (t*E and Y > 0.5)'=rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'>0.5)],
-         'Uncovered Most Likely (T*e and Y > 0.5)'=rownames(ND)[(ND$'T*e'>0.5)&(ND$'Outcome'>0.5)],
-         'Uncovered Least Likely (t*e and Y > 0.5)'=rownames(ND)[(ND$'t*e'>0.5)&(ND$'Outcome'>0.5)],
-         'Inconsistent Most Likely (T*E and Y < 0.5)'=rownames(ND)[(ND$'T*E'>0.5)&(ND$'Outcome'<0.5)],
-         'Inconsistent Least Likely (t*E and Y < 0.5)'=rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'<0.5)],
-         'Consistent Most Likely (T*e and Y < 0.5)'=rownames(ND)[(ND$'T*e'>0.5)&(ND$'Outcome'<0.5)],
-         'Consistent Least Likely (t*e and Y < 0.5)'=rownames(ND)[(ND$'t*e'>0.5)&(ND$'Outcome'<0.5)])
+    CTE <- list('TEY'=list('Intersection'='Covered Most Likely (T*E and Y > 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases:',
+                                          sum((ND$'T*E'>0.5)&(ND$'Outcome'>0.5)),"/",nrow(ND),
+                                          "=", round((sum((ND$'T*E'>0.5)&(ND$'Outcome'>0.5))/nrow(ND))*100, digits=2), "%"), 
+                           'CaseNames'= if(sum((ND$'T*E'>0.5)&(ND$'Outcome'>0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'T*E'>0.5)&(ND$'Outcome'>0.5)]}),
+                'tEY'=list('Intersection'='Covered Least Likely (t*E and Y > 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases:',
+                                          sum((ND$'t*E'>0.5)&(ND$'Outcome'>0.5)),"/",nrow(ND),
+                                          "=", round((sum((ND$'t*E'>0.5)&(ND$'Outcome'>0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'t*E'>0.5)&(ND$'Outcome'>0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'>0.5)]}),
+                'TeY'=list('Intersection'='Uncovered Most Likely (T*e and Y > 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
+                                          sum((ND$'T*e'>0.5)&(ND$'Outcome'>0.5)),"/", nrow(ND),
+                                          "=", round((sum((ND$'T*e'>0.5)&(ND$'Outcome'>0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'T*e'>0.5)&(ND$'Outcome'>0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'T*e'>0.5)&(ND$'Outcome'>0.5)]}),
+                'teY'=list('Intersection'='Uncovered Least Likely (t*e and Y > 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
+                                          sum((ND$'t*e'>0.5)&(ND$'Outcome'>0.5)),"/", nrow(ND),
+                                          "=", round((sum((ND$'t*e'>0.5)&(ND$'Outcome'>0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'t*e'>0.5)&(ND$'Outcome'>0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'t*e'>0.5)&(ND$'Outcome'>0.5)]}),
+                'TEy'=list('Intersection'='Inconsistent Most Likely (T*E and Y < 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
+                                          sum((ND$'T*E'>0.5)&(ND$'Outcome'<0.5)),"/", nrow(ND),
+                                          "=", round((sum((ND$'T*E'>0.5)&(ND$'Outcome'<0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'T*E'>0.5)&(ND$'Outcome'<0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'T*E'>0.5)&(ND$'Outcome'<0.5)]}),
+                'tEy'=list('Intersection'='Inconsistent Least Likely (t*E and Y < 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
+                                          sum((ND$'t*E'>0.5)&(ND$'Outcome'<0.5)),"/", nrow(ND),
+                                          "=", round((sum((ND$'t*E'>0.5)&(ND$'Outcome'<0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'t*E'>0.5)&(ND$'Outcome'<0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'<0.5)]}),
+                'Tey'=list('Intersection'='Consistent Most Likely (T*e and Y < 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
+                                          sum((ND$'T*e'>0.5)&(ND$'Outcome'<0.5)),"/", nrow(ND),
+                                          "=", round((sum((ND$'T*e'>0.5)&(ND$'Outcome'<0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'T*e'>0.5)&(ND$'Outcome'<0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'T*e'>0.5)&(ND$'Outcome'<0.5)]}),
+                'tey'=list('Intersection'='Consistent Least Likely (t*e and Y < 0.5)', 
+                           'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
+                                          sum((ND$'t*e'>0.5)&(ND$'Outcome'<0.5)),"/", nrow(ND),
+                                          "=", round((sum((ND$'t*e'>0.5)&(ND$'Outcome'<0.5))/nrow(ND))*100, digits=2), "%"),
+                           'CaseNames'= if(sum((ND$'t*e'>0.5)&(ND$'Outcome'<0.5)) == 0) {'No cases in this intersection'} 
+                           else {rownames(ND)[(ND$'t*e'>0.5)&(ND$'Outcome'<0.5)]}))
+    class(CTE) <- 'casestheoryeval'
+    return(CTE)
   }
