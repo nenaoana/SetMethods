9f61d59b14daabcb10285bbf135f77766517726b
nenaoana <nena.oana@yahoo.com>
2017-03-02 13:10:40 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
-.onLoad <- function(...) {
-  packageStartupMessage("To cite package SetMethods in publications use:
+.onAttach <- function(...) {
+  packageStartupMessage("
+
+To cite package SetMethods in publications use:
   
   Juraj Medzihorsky, Ioana-Elena Oana, Mario Quaranta and Carsten Q. Schneider (2016).
   SetMethods: Functions for Set-Theoretic Multi-Method Research and Advanced QCA. R package
