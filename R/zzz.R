022ca1c194f13655d5d021a8433f86d0eccf851f
nenaoana <nena.oana@yahoo.com>
2017-03-02 13:05:05 +0100

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+.onLoad <- function(...) {
+  packageStartupMessage("To cite package ???SetMethods??? in publications use:/n /n Juraj Medzihorsky, Ioana-Elena Oana, Mario Quaranta and Carsten Q. Schneider (2016).
+  SetMethods: Functions for Set-Theoretic Multi-Method Research and Advanced QCA. R package
+  version 2.0./n")
+}
