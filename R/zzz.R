e053b7962d474d1ef52db7e6c194494fc0c803c6
nenaoana <nena.oana@yahoo.com>
2018-06-14 08:28:19 +0300

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,10 @@
 .onAttach <- function(...) {
-  packageStartupMessage("To cite package SetMethods in publications use: 
+  packageStartupMessage("
 
-  Juraj Medzihorsky, Ioana-Elena Oana, Mario Quaranta and Carsten Q. Schneider (2018).    
-  SetMethods: Functions for Set-Theoretic Multi-Method Research and Advanced QCA. R package
-  version 2.3.1
+  To cite package SetMethods in publications use: 
+
+  Ioana-Elena Oana and Carsten Q. Schneider (2018). SetMethods: an Add-on Package for Advanced QCA. 
+  The R Journal. https://journal.r-project.org/archive/2018/RJ-2018-031/index.html
   
   ")
 }
