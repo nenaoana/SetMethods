6ee5a1c95df076d9616fa27420cf3ba8793fa3b6
nenaoana <nena.oana@yahoo.com>
2018-11-16 12:02:36 +0100

    Add files via upload

--- R/cases.theory.evaluation.R
+++ R/cases.theory.evaluation.R
@@ -24,9 +24,9 @@ cases.theory.evaluation <-
                                           "=", round((sum((ND$'~T*E'>0.5)&(ND$'Outcome'>0.5))/nrow(ND))*100, digits=2), "%"),
                            'CaseNo2'=paste('Cases in the intersection/Total number of cases Y > 0.5: ',
                                           sum((ND$'~T*E'>0.5)&(ND$'Outcome'>0.5)),"/",sum((ND$'Outcome'>0.5)),
-                                          "=", round((sum((ND$'t*E'>0.5)&(ND$'Outcome'>0.5))/sum((ND$'Outcome'>0.5)))*100, digits=2), "%"),
+                                          "=", round((sum((ND$'~T*E'>0.5)&(ND$'Outcome'>0.5))/sum((ND$'Outcome'>0.5)))*100, digits=2), "%"),
                            'CaseNames'= if(sum((ND$'~T*E'>0.5)&(ND$'Outcome'>0.5)) == 0) {'No cases in this intersection'} 
-                           else {rownames(ND)[(ND$'t*E'>0.5)&(ND$'Outcome'>0.5)]}),
+                           else {rownames(ND)[(ND$'~T*E'>0.5)&(ND$'Outcome'>0.5)]}),
                 'T~EY'=list('Intersection'='Uncovered Most Likely (T*~E and Y > 0.5)',
                            'Boolean'=paste('Boolean Expression:', INT$Te),
                            'CaseNo'=paste('Cases in the intersection/Total number of cases: ',
